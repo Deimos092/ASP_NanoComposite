e1f28f85649cc26dff99b0a194bc31efd93e3968
DemDemich <koreneffdim1@gmail.com>
2019-04-06 13:00:46 +0500

    Model

--- ASP_NanoComposite/Controllers/HomeController.cs
+++ ASP_NanoComposite/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CodeFirst;
 
 namespace ASP_NanoComposite.Controllers
 {
@@ -10,6 +11,23 @@ namespace ASP_NanoComposite.Controllers
 	{
 		public ActionResult Index()
 		{
+            using (Context cont = new Context())
+            {
+                /*User u = new User();
+                u.SubModel = new SubscriptionModel();
+                u.Login = "Test";
+                cont.Users.Add(u);
+                cont.SaveChanges();*/
+                /*var u = cont.Users.Where(e => e.Login == "Test");
+                foreach (var item in u)
+                {
+                    item.SubModel.NumberOfProj = 5;
+                }*/
+                SubscriptionModel s = new SubscriptionModel();
+                s.NumberOfShared = 1;
+                cont.SubModel.Add(s);
+                cont.SaveChanges();
+            }
             return View();
 		}
 
---
+++ ASP_NanoComposite/Models/Model.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CodeFirst
+{
+    public class Context : DbContext
+    {
+        public Context() : base("Test")
+        { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<SubscriptionModel> SubModel { get; set; }
+    }
+    public class SubscriptionModel
+    {
+        public int SubscriptionModelID { get; set; }
+        public int NumberOfProj { get; set; }
+        public int NumberOfShared { get; set; }
+    }
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public SubscriptionModel SubModel { get; set; }
+        public string APIKey { get; set; }
+    }
+}
