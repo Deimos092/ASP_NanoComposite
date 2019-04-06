5918bb110a2385482a2fce96ff085c15b35e2979
DemDemich <41775352+DemDemich@users.noreply.github.com>
2019-04-06 14:48:26 +0500

    Added models

--- ASP_NanoComposite/Models/Model.cs
+++ ASP_NanoComposite/Models/Model.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 
-namespace CodeFirst
+namespace ASP_NanoComposite.Models//CodeFirst //просто для лучшего понимания заменил на Models
 {
     public class Context : DbContext
     {
@@ -12,14 +12,26 @@ namespace CodeFirst
         { }
         public DbSet<User> Users { get; set; }
         public DbSet<SubscriptionModel> SubModel { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<Share> Shares { get; set; }
+        public DbSet<ProjectMaterials> ProjectMaterials { get; set; }
     }
-    public class SubscriptionModel
+    public class SubscriptionModel //модель подписки
     {
         public int SubscriptionModelID { get; set; }
         public int NumberOfProj { get; set; }
         public int NumberOfShared { get; set; }
+        public decimal SubCost { get; set; }
     }
-    public class User
+    public class Project //проект
+    {
+        public int ProjectID { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectDescription { get; set; }
+        public DateTime ProjectDate { get; set; }
+    }
+    public class User //пользователь
     {
         public int UserID { get; set; }
         public string Login { get; set; }
@@ -27,5 +39,34 @@ namespace CodeFirst
         public string Password { get; set; }
         public SubscriptionModel SubModel { get; set; }
         public string APIKey { get; set; }
+    }
+    public class Share
+    {
+        public int ShareID { get; set; }
+        public int ProjectToShareID { get; set; }
+        public int OwnerID { get; set; }//владелец
+        public int SharedID { get; set; }//кому дает доступ
+        public bool isRead { get; set; }
+        public bool isWrite { get; set; }
+    }
+    public class ProjectMaterials
+    {
+        public int ProjectMaterialsID { get; set; }
+        public int ProjectID { get; set; }
+        public int MaterialID { get; set; }
+    }
+    public class Material
+    {
+        public int MaterialID { get; set; }
+        public string Name { get; set; }
+        public decimal Hardness { get; set; }
+        public decimal Elasticity { get; set; }
+        public decimal StrengthBeyond { get; set; }
+        public decimal Density { get; set; }
+        public decimal HeatCapacity { get; set; }
+        public decimal ThermalConduct { get; set; }
+        public decimal ThermalExpansion { get; set; }
+        public decimal Percent { get; set; }
+        public bool isMatrix { get; set; }
     }
 }
